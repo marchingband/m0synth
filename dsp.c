a978dfce51244638adb6cdf8797c51c53c02c15f
Andrew March <youthclubrecords@gmail.com>
2023-05-22 12:25:50 -0700

    ?

--- dsp.c
+++ dsp.c
@@ -73,49 +73,49 @@ void dsp_init(size_t buf_size){
 
     voices[0].on = false;
     voices[0].note = 0;
-    voices[0].pitch = &dsp.fHslider2;
+    voices[0].pitch = &dsp.fHslider0;
     voices[0].gain = &dsp.fVbargraph0;
     voices[0].gate = &dsp.fButton0;
 
     voices[1].on = false;
     voices[1].note = 0;
-    voices[1].pitch = &dsp.fHslider3;
+    voices[1].pitch = &dsp.fHslider1;
     voices[1].gain = &dsp.fVbargraph1;
     voices[1].gate = &dsp.fButton1;
 
     voices[2].on = false;
     voices[2].note = 0;
-    voices[2].pitch = &dsp.fHslider4;
+    voices[2].pitch = &dsp.fHslider2;
     voices[2].gain = &dsp.fVbargraph2;
     voices[2].gate = &dsp.fButton2;
 
     voices[3].on = false;
     voices[3].note = 0;
-    voices[3].pitch = &dsp.fHslider5;
+    voices[3].pitch = &dsp.fHslider3;
     voices[3].gain = &dsp.fVbargraph3;
     voices[3].gate = &dsp.fButton3;
 
     voices[4].on = false;
     voices[4].note = 0;
-    voices[4].pitch = &dsp.fHslider6;
+    voices[4].pitch = &dsp.fHslider4;
     voices[4].gain = &dsp.fVbargraph4;
     voices[4].gate = &dsp.fButton4;
 
     voices[5].on = false;
     voices[5].note = 0;
-    voices[5].pitch = &dsp.fHslider7;
+    voices[5].pitch = &dsp.fHslider5;
     voices[5].gain = &dsp.fVbargraph5;
     voices[5].gate = &dsp.fButton5;
 
     voices[6].on = false;
     voices[6].note = 0;
-    voices[6].pitch = &dsp.fHslider8;
+    voices[6].pitch = &dsp.fHslider6;
     voices[6].gain = &dsp.fVbargraph6;
     voices[6].gate = &dsp.fButton6;
 
     voices[7].on = false;
     voices[7].note = 0;
-    voices[7].pitch = &dsp.fHslider9;
+    voices[7].pitch = &dsp.fHslider7;
     voices[7].gain = &dsp.fVbargraph7;
     voices[7].gate = &dsp.fButton7;
 
@@ -171,12 +171,12 @@ void stop(uint8_t note){
 
 void set_filter1(uint8_t val){
     float freq = (float)val / 127.0f; // 0 ~ 1
-    dsp.fHslider0 = freq;
+    dsp.fHslider8 = freq;
 }
 
 void set_filter2(uint8_t val){
     float freq = ((float)val / 127.0f) * 2000; // 0 ~ 2000
-    dsp.fHslider1 = freq;
+    dsp.fHslider9 = freq;
 }
 
 void set_filter3(uint8_t val){
