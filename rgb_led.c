74112b967b3b0d642e60f00c13b9911019b41fd0
Andrew March <youthclubrecords@gmail.com>
2023-07-06 00:55:46 -0700

    test rgb

--- rgb_led.c
+++ rgb_led.c
@@ -4,8 +4,8 @@
 
 #include "board.h"
 
-#define RGB_CLOCK_DIV 39
-// #define RGB_CLOCK_DIV 14
+// #define RGB_CLOCK_DIV 39
+#define RGB_CLOCK_DIV 12
 #define LED_GPIO GPIO_PIN_10
 #define RGB_TIMER_COMP_ID TIMER_COMP_ID_0
 
@@ -31,7 +31,7 @@ void rgb_led_init(){
     cfg.clock_source = TIMER_CLKSRC_XTAL;
     cfg.clock_div = RGB_CLOCK_DIV;
     cfg.trigger_comp_id = RGB_TIMER_COMP_ID;
-    cfg.comp0_val = 1000000;
+    cfg.comp0_val = 1;
     // cfg.comp1_val = 2000000;
     // cfg.comp2_val = 3000000;
     cfg.preload_val = 0;
@@ -47,61 +47,61 @@ void timer0_isr(int irq, void *arg)
     bool status = bflb_timer_get_compint_status(timer0, RGB_TIMER_COMP_ID);
     if (status) {
         bflb_timer_compint_clear(timer0, RGB_TIMER_COMP_ID);
-        printf("isr");
-        // if(p < NUM_BIT_BITS)
-        // {
-        //     uint32_t val = rgb_sig[p];
-        //     p += 1;
-        //     if(val == 0)
-        //     {
-        //         bflb_gpio_reset(gpio, LED_GPIO);
-        //     }
-        //     else
-        //     {
-        //         bflb_gpio_set(gpio, LED_GPIO);
-        //     }
-        // }
-        // else
-        // {
-        //     p = 0;
-        //     bflb_timer_stop(timer0);
-        //     bflb_irq_disable(timer0->irq_num);
-        //     bflb_gpio_reset(gpio, LED_GPIO);
-        //     bflb_mtimer_delay_us(90);
-        //     printf("done\n\n");
-        // }
-        if(test == 0)
+        // printf("isr");
+        if(p < NUM_BIT_BITS)
         {
-            bflb_gpio_reset(gpio, LED_GPIO);
-            test = 1;
+            uint32_t val = rgb_sig[p];
+            p += 1;
+            if(val == 0)
+            {
+                bflb_gpio_reset(gpio, LED_GPIO);
+            }
+            else
+            {
+                bflb_gpio_set(gpio, LED_GPIO);
+            }
         }
         else
         {
-            bflb_gpio_set(gpio, LED_GPIO);
-            test = 0;
+            p = 0;
+            bflb_timer_stop(timer0);
+            bflb_irq_disable(timer0->irq_num);
+            bflb_gpio_reset(gpio, LED_GPIO);
+            bflb_mtimer_delay_us(90);
+            printf("done\n\n");
         }
+        // if(test == 0)
+        // {
+        //     bflb_gpio_reset(gpio, LED_GPIO);
+        //     test = 1;
+        // }
+        // else
+        // {
+        //     bflb_gpio_set(gpio, LED_GPIO);
+        //     test = 0;
+        // }
     }
 }
 
 void rgb_send_color(void){
     // print the array
-    // for(int i=0; i<24; i++)
-    // {
-    //     int bit = (color >> i) & 0x1;
-    //     int pos = i * 3;
-    //     if( bit == 0 )
-    //     {
-    //         rgb_sig[pos]     = 1;
-    //         rgb_sig[pos + 1] = 0;
-    //         rgb_sig[pos + 2] = 0;
-    //     }
-    //     else
-    //     {
-    //         rgb_sig[pos]     = 1;
-    //         rgb_sig[pos + 1] = 1;
-    //         rgb_sig[pos + 2] = 0;
-    //     }
-    // }
+    for(int i=0; i<24; i++)
+    {
+        int bit = (color >> i) & 0x1;
+        int pos = i * 3;
+        if( bit == 0 )
+        {
+            rgb_sig[pos]     = 1;
+            rgb_sig[pos + 1] = 0;
+            rgb_sig[pos + 2] = 0;
+        }
+        else
+        {
+            rgb_sig[pos]     = 1;
+            rgb_sig[pos + 1] = 1;
+            rgb_sig[pos + 2] = 0;
+        }
+    }
 
     // // log the array
     // for(int i=0; i< NUM_BIT_BITS; i++)
