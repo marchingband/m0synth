fe432759ddbf2d8fb4ec5f6502e243e56fdde7f9
Andrew March <youthclubrecords@gmail.com>
2023-07-10 20:27:14 -0700

    test rgb

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include "bflb_uart.h"
 
 #include "bl616_glb.h"
+#include "bflb_i2s.h"
 #include "bl616_glb_gpio.h"
 #include "bl616_irq.h"
 #include "board.h"
@@ -101,10 +102,8 @@ void audac_gpio_init(void)
     gpio = bflb_device_get_by_name("gpio");
 
     /* audac pwm output mode */
-    // bflb_gpio_init(gpio, GPIO_PIN_14, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
-    // bflb_gpio_init(gpio, GPIO_PIN_15, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
-    bflb_gpio_init(gpio, GPIO_PIN_12, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
-    bflb_gpio_init(gpio, GPIO_PIN_13, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
+    bflb_gpio_init(gpio, GPIO_PIN_14, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
+    bflb_gpio_init(gpio, GPIO_PIN_15, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
 
     // bflb_gpio_init(gpio, GPIO_PIN_22, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
     // bflb_gpio_init(gpio, GPIO_PIN_23, GPIO_FUNC_AUDAC_PWM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_2);
@@ -341,6 +340,74 @@ void gpio_sig_init(void)
 
 }
 
+void i2s_init(void)
+{
+    struct bflb_device_s *i2s0;
+    struct bflb_device_s *dma0_ch0;
+    static ATTR_NOCACHE_NOINIT_RAM_SECTION uint16_t tx_buffer[256] __ALIGNED(4);
+    static volatile uint8_t dma_tc_flag0 = 0;
+    for (i = 0; i < 256; i++) {
+        tx_buffer[i] = i % 2 ? 10000 : 50000;
+    }
+    struct bflb_i2s_config_s i2s_cfg = {
+        .bclk_freq_hz = 32000 * 16 * 2, /* bclk = Sampling_rate * frame_width * channel_num */
+        .role = I2S_ROLE_MASTER,
+        .format_mode = I2S_MODE_LEFT_JUSTIFIED,
+        .channel_mode = I2S_CHANNEL_MODE_NUM_2,
+        .frame_width = I2S_SLOT_WIDTH_16,
+        .data_width = I2S_SLOT_WIDTH_16,
+        .fs_offset_cycle = 0,
+        .tx_fifo_threshold = 0,
+        .rx_fifo_threshold = 0,
+    };
+        struct bflb_dma_channel_config_s tx_config = {
+        .direction = DMA_MEMORY_TO_PERIPH,
+        .src_req = DMA_REQUEST_NONE,
+        .dst_req = DMA_REQUEST_I2S_TX,
+        .src_addr_inc = DMA_ADDR_INCREMENT_ENABLE,
+        .dst_addr_inc = DMA_ADDR_INCREMENT_DISABLE,
+        .src_burst_count = DMA_BURST_INCR1,
+        .dst_burst_count = DMA_BURST_INCR1,
+        .src_width = DMA_DATA_WIDTH_16BIT,
+        .dst_width = DMA_DATA_WIDTH_16BIT,
+    };
+
+    struct bflb_dma_channel_config_s rx_config = {
+        .direction = DMA_PERIPH_TO_MEMORY,
+        .src_req = DMA_REQUEST_I2S_RX,
+        .dst_req = DMA_REQUEST_NONE,
+        .src_addr_inc = DMA_ADDR_INCREMENT_DISABLE,
+        .dst_addr_inc = DMA_ADDR_INCREMENT_ENABLE,
+        .src_burst_count = DMA_BURST_INCR1,
+        .dst_burst_count = DMA_BURST_INCR1,
+        .src_width = DMA_DATA_WIDTH_16BIT,
+        .dst_width = DMA_DATA_WIDTH_16BIT,
+    };
+
+    board_i2s_gpio_init();
+    i2s0 = bflb_device_get_by_name("i2s0");
+    bflb_i2s_init(i2s0, &i2s_cfg);
+    bflb_i2s_link_txdma(i2s0, true);
+
+    printf("\n\ri2s dma test\n\r");
+    dma0_ch0 = bflb_device_get_by_name("dma0_ch0");
+    bflb_dma_channel_init(dma0_ch0, &tx_config);
+    bflb_dma_channel_irq_attach(dma0_ch0, dma0_ch0_isr, NULL);
+
+    tx_transfers[0].src_addr = (uint32_t)tx_buffer;
+    tx_transfers[0].dst_addr = (uint32_t)DMA_ADDR_I2S_TDR;
+    tx_transfers[0].nbytes = sizeof(tx_buffer);
+
+    uint32_t num = bflb_dma_channel_lli_reload(dma0_ch0, tx_llipool, 1, tx_transfers, 1);
+    bflb_dma_channel_lli_link_head(dma0_ch0, tx_llipool, num);
+    bflb_dma_channel_start(dma0_ch0);
+
+    bflb_i2s_feature_control(i2s0, I2S_CMD_DATA_ENABLE, I2S_CMD_DATA_ENABLE_TX | I2S_CMD_DATA_ENABLE_RX);
+    printf("\n\rtest end\n\r");
+
+
+}
+
 int main(void)
 {
     /* board init */
@@ -377,6 +444,8 @@ int main(void)
 
     /* usb midi host */
     usb_midi_start();
+
+    i2s_init();
 
 
     // // printf("uart is irq %d with prio %d\n", uart_int_num, uart_prio);
